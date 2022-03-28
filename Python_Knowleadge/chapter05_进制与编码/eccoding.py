7dbb9ef65f54e2a7cbebf10ca8f0dd42f2683359
ChuanmingXie <ChuanmingXie@outlook.com>
2022-03-28 20:18:08 +0800

    Python_知识点-进制与编码-注释

--- Python_Knowleadge/chapter05_进制与编码/eccoding.py
+++ Python_Knowleadge/chapter05_进制与编码/eccoding.py
@@ -25,9 +25,12 @@ def binencode():
 
     # 将内存文字写入文本
     text = "功高不自居，\n名高不自誉，\n位高不自傲"
-    data = text.encode("utf-8")  # 打开一个文件
-    file_object = open("log.txt", mode="wb")  # 在文件中写内容
-    file_object.write(data)  # 关闭文件 file_object.close()
+    # 打开一个文件
+    data = text.encode("utf-8")
+    # 在文件中写内容
+    file_object = open("log.txt", mode="wb") 
+    # 关闭文件 file_object.close()
+    file_object.write(data)
 
 def nameencode():
     name = '云海鲸明'
